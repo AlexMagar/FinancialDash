aa5000682e22dc225329ac81898dbaf00b443c9b
AlexMagar <lx.mgr07@gmail.com>
2024-05-17 17:03:15 +1000

    feat: search param for query selector

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,8 +4,16 @@ import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
+import Table from "@/app/ui/invoices/table";
 
-export default function Invoice(){
+export default function Invoice({searchParams,
+}:{searchParams?:{
+    query?:string;
+    page?: string;
+}
+}){
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -15,9 +23,9 @@ export default function Invoice(){
                 <Search placeholder="Search invoices....." />
                 <CreateInvoice />
             </div>
-            {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
                 <Table query={query} currentPage={currentPage} />
-            </Suspense> */}
+            </Suspense>
             <div className="mt-5 flex w-full justify-center">
                 {/* <Pagination totalPages={totalPages} /> */}
             </div>
